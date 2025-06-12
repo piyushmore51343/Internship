6cba2c891e362ce6d556c764e077c26957014156
piyushmore51343 <piyushmore51434@gmail.com>
2025-06-12 16:55:09 +0530

    Set operation

---
+++ Day_8/set.py
@@ -0,0 +1,14 @@
+set1 = {1,2,3,4}
+set2 = {3,4,5,6}
+
+#common elements
+print("common elements",set1.intersection(set2))
+
+#elements in set1 but not in set2
+print("elements in set1 but not in set2",set1 - set2)
+
+#elements in set2 but not in set1
+print("elements in set2 but not in set1",set2 - set1)
+
+
+
