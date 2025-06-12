0b6342976631f6c59e991c8a97f9c722fd777258
piyushmore51343 <piyushmore51434@gmail.com>
2025-06-12 17:02:13 +0530

    Set Operation

--- Day_8/set.py
+++ Day_8/set.py
@@ -1,14 +1,16 @@
-set1 = {1,2,3,4}
-set2 = {3,4,5,6}
+# set1 = {1,2,3,4}
+# set2 = {3,4,5,6}
 
-#common elements
-print("common elements",set1.intersection(set2))
+# #common elements
+# print("common elements",set1.intersection(set2))
 
-#elements in set1 but not in set2
-print("elements in set1 but not in set2",set1 - set2)
+# #elements in set1 but not in set2
+# print("elements in set1 but not in set2",set1 - set2)
 
-#elements in set2 but not in set1
-print("elements in set2 but not in set1",set2 - set1)
+# #elements in set2 but not in set1
+# print("elements in set2 but not in set1",set2 - set1)
 
-
+tup = ([1,2],[3,4])
+tup[0][1]=(99)
+print(tup)
 
