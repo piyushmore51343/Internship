686c5bf81c8ed947c6c30297f4ee4887f777fe2a
piyushmore51343 <piyushmore51434@gmail.com>
2025-06-12 16:39:10 +0530

    Tuple

---
+++ Day_8/repeated.py
@@ -0,0 +1,10 @@
+my_tuple = (1,89,100,22,45,67,89,100)
+
+my_list = []
+for i in range(len(my_tuple)):
+    count = my_tuple.count(my_tuple[i])
+    if count > 1 and my_tuple[i] not in my_list:
+        my_list.append(my_tuple[i])
+
+
+print(my_list)
